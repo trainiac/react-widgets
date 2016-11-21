32d81e8d5e3f5d30707db5cb7d07950466a42f8c
trainiac <adkison.adrian@gmail.com>
2016-11-21 08:10:55 -0800

    use new config structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const devServerConfig = require('trainiac-webpack-configs/devserver')
+const devServerConfig = require('webpack-config-trainiac/devserver')
 const buildPath = path.join(__dirname, 'build')
 const srcPath = path.join(__dirname, 'client')
 
